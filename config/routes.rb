e4f5f6cf637cc5d9d8c1dcd77c85199e713163ca
miopanda <114249779+miopanda@users.noreply.github.com>
2023-06-22 11:48:02 +0900

    cardモデルとコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   devise_for :users
   root to: 'items#index'
   
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
 end
